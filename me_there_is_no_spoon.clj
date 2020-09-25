4e9592b775ebcd0855d262d52fd53ffa46b412b6
Vincent <ravera.vincent@gmail.com>
2020-09-25 18:58:09 +0200

    There is no spoon 8/8

--- me_there_is_no_spoon.clj
+++ me_there_is_no_spoon.clj
@@ -5,11 +5,40 @@
 
 (defn output [msg] (println msg) (flush))
 (defn debug [msg] (binding [*out* *err*] (println msg) (flush)))
+(defn format-node [x]
+  (str (x 0) " " (x 1)))
 (defn doesNodeExist [lon X Y]
   (if (some #(= [X Y] %) lon)
     (do [X Y])
     (do [-1 -1]))
   )
+(defn getDoValue [lon x]
+  (let [node
+        (some
+         (fn [a] (when (and
+                        (= (x 0) (a 0))
+                        (< (x 1) (a 1)) )
+                   a ) )
+         lon)]
+    (if (not node)
+      (do [-1 -1])
+      (do node))
+    )
+  )
+(defn getRiValue [lon x]
+  (let [node
+        (some
+         (fn [a] (when (and
+                        (< (x 0) (a 0))
+                        (= (x 1) (a 1)) )
+                   a ) )
+         lon)]
+    (if (not node)
+      (do [-1 -1])
+      (do node))
+    )
+  )
+
 
 (defn -main [& args]
   (let [width (read) height (read) _ (read-line)]
@@ -20,26 +49,23 @@
         (when (< i height)
           (let [line (into [] (clojure.string/split (read-line) #"" ))]
             ;; line: width characters, each either 0 or .
-            (debug (str i" -> " line))
+            ;; (debug (str i" -> " line))
             (var-set list-of-in (into (var-get list-of-in) line))
             (loop [j 0]
               (when (< j width)
                 (when (= "0" (get line j))
-                  (var-set list-of-node (conj (var-get list-of-node) [i j]))
+                  (var-set list-of-node (conj (var-get list-of-node) [j i]))
                   )
               (recur (inc j))))
             (recur (inc i)))))
 
       ;; (debug "Debug messages...")
-      (debug (var-get list-of-node))
+      ;; (debug (var-get list-of-node))
       (var-set list-of-out
                (for [node (var-get list-of-node)]
-                 (str (node 0) " "
-                      (node 1) " "
-                      ((doesNodeExist (var-get list-of-node) (+ 1 (get node 0)) (get node 1)) 0) " "
-                      ((doesNodeExist (var-get list-of-node) (+ 1 (get node 0)) (get node 1)) 1) " "
-                      ((doesNodeExist (var-get list-of-node) (get node 0) (+ 1 (get node 1))) 0) " "
-                      ((doesNodeExist (var-get list-of-node) (get node 0) (+ 1 (get node 1))) 1)
+                 (str (format-node node) " "
+                      (format-node (getRiValue (var-get list-of-node) node)) " "
+                      (format-node (getDoValue (var-get list-of-node) node))
                       )
                  ))
       (doseq [out (var-get list-of-out)]
